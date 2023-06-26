d500e221f965569f0692cd8c78d0f83fca43789b
EvilSeal1980 <evilseal09@gmail.com>
2023-06-27 02:12:17 +0530

    Build Delete and Display feature

--- Week-2-Assignments/02-nodejs/todoServer.js
+++ Week-2-Assignments/02-nodejs/todoServer.js
@@ -40,104 +40,79 @@
   Testing the server - run `npm run test-todoServer` command in terminal
  */
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-const app = express();
-const path = require("path");
-const cors = require("cors");
-const port = 3000;
-
-app.use(cors);
-
-app.use(bodyParser.json());
-
-let todos = new Map();
-fs.readFile("./todos.json", "utf8", (err, data) => {
-  if (!err) {
-    todos = new Map(JSON.parse(data));
+  const express = require('express');
+  const bodyParser = require('body-parser');
+  const cors = require("cors");
+  const app = express();
+  
+  app.use(cors());
+  
+  app.use(bodyParser.json());
+  
+  let todos = [];
+  
+  function findIndex(arr, id) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].id === id) return i;
+    }
+    return -1;
   }
-});
-
-function saveTodosToFile() {
-  fs.writeFile(path, JSON.stringify([...todos]), "utf8", (err) => {
-    if (err) {
-      console.error("Error saving todos:", err);
+  
+  function removeAtIndex(arr, index) {
+    let newArray = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (i !== index) newArray.push(arr[i]);
+    }
+    return newArray;
+  }
+  
+  app.get('/todos', (req, res) => {
+    res.json(todos);
+  });
+  
+  app.get('/todos/:id', (req, res) => {
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      res.json(todos[todoIndex]);
     }
   });
-}
-
-const generateId = () => {
-  return todos.size + 1;
-};
-
-app.get("/todos", (req, res) => {
-  const todosArray = [...todos.values()];
-  res.status(200).json(todosArray);
-});
-
-app.get("/todos/:id", (req, res) => {
-  const todo = todos.get(parseInt(req.params.id));
-  if (!todo) {
-    res.status(404).json({ error: "Todo item not found" });
-  } else {
-    res.status(200).json(todo);
-  }
-});
-
-app.post("/todos", (req, res) => {
-  const newTodo = {
-    id: generateId(),
-    title: req.body.title,
-    description: req.body.description,
-    completed: false,
-  };
-
-  todos.set(newTodo.id, newTodo);
-  saveTodosToFile();
-  res.status(201).json({ id: newTodo.id });
-});
-
-// Route: Update an existing todo item by ID
-app.put("/todos/:id", (req, res) => {
-  const todo = todos.get(parseInt(req.params.id));
-  if (!todo) {
-    res.status(404).json({ error: "Todo item not found" });
-  } else {
-    const updatedTodo = {
-      ...todo,
-      ...req.body,
+  
+  app.post('/todos', (req, res) => {
+    const newTodo = {
+      id: Math.floor(Math.random() * 1000000), // unique random id
+      title: req.body.title,
+      description: req.body.description
     };
-    todos.set(updatedTodo.id, updatedTodo);
-    saveTodosToFile();
-    res.status(200).send();
-  }
-});
-
-app.delete("/todos/:id", (req, res) => {
-  const todo = todos.get(parseInt(req.params.id));
-  if (!todo) {
-    res.status(404).json({ error: "Todo item not found" });
-  } else {
-    todos.delete(todo.id);
-    saveTodosToFile();
-    res.status(200).send();
-  }
-});
-
-
-app.use((req, res) => {
-  res.status(404).json({ error: "Route not found" });
-});
-
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
-});
-
-
-module.exports = app;
-
-
-
-
-
+    todos.push(newTodo);
+    res.status(201).json(newTodo);
+  });
+  
+  app.put('/todos/:id', (req, res) => {
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      todos[todoIndex].title = req.body.title;
+      todos[todoIndex].description = req.body.description;
+      res.json(todos[todoIndex]);
+    }
+  });
+  
+  app.delete('/todos/:id', (req, res) => {
+    const todoIndex = findIndex(todos, parseInt(req.params.id));
+    if (todoIndex === -1) {
+      res.status(404).send();
+    } else {
+      todos = removeAtIndex(todos, todoIndex);
+      res.status(200).send();
+    }
+  });
+  
+  // for all other routes, return 404
+  app.use((req, res, next) => {
+    res.status(404).send();
+  });
+  
+  app.listen(3000);
