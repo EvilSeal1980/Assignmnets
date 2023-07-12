1bae3ddf7276555ad19c44749e7dcbe260bbb22a
EvilSeal1980 <evilseal09@gmail.com>
2023-07-12 23:47:26 +0530

    Build login page

--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Login.jsx
@@ -1,18 +1,69 @@
-import React from "react";
+import { Button, Card, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
-/// File is incomplete. You need to add input boxes to take input for users to login.
-function Login() {
-    const [email, setEmail] = React.useState("");
-
-    return <div>
-        <h1>Login to admin dashboard</h1>
-        <br/>
-        Email - <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
-        <button>Login</button>
-        <br/>
-        New here? <a href="/register">Register</a>
-    </div>
+const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    return (
+      <>
+        <div style={{height:"100vh", display:"flex", flexDirection:"column", justifyContent:"center"}}>
+          <div style={{display:"flex", justifyContent:"center"}}>
+            <Typography variant="h6">
+              Welcome to Coursera. Sign in below
+            </Typography>
+          </div>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Card variant="outlined" style={{ width: 400, padding: 20 }}>
+              <TextField
+                onChange={(e) => setEmail(e.target.value)}
+                fullWidth={true}
+                label="Email"
+                variant="outlined"
+              />
+              <br />
+              <br />
+              <TextField
+                onChange={(e) => setPassword(e.target.value)}
+                fullWidth={true}
+                label="Password"
+                variant="outlined"
+              />
+              <br />
+              <br />
+              <Button
+              size="large"
+              variant="contained"
+              onClick= {async (e) => {
+                  e.preventDefault();
+                  const existingUser = {
+                      username: email,
+                      password: password
+                  }
+                  const response = await fetch("http://localhost:3000/admin/login", {
+                      method: "POST",
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify(existingUser),
+                    });
+  
+                    if(response.ok) {
+                      const resData = await response.json();
+                      const token = resData.token;
+                      localStorage.setItem("token", token);
+                    }
+                    else {
+                        console.log("Signin failed");
+                    }
+              }}>
+                  Sign In
+              </Button>
+            </Card>
+          </div>
+        </div>
+      </>
+    );
 }
 
-export default Login;
+export default Login
--- week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/components/Register.jsx
@@ -1,16 +1,73 @@
-import React from "react";
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import {Card, Typography} from "@mui/material";
+import {useState} from "react";
 
-/// File is incomplete. You need to add input boxes to take input for users to register.
-function Register() {
-    const [email, setEmail] = React.useState("");
+function Signup() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     return <div>
-        <h1>Register to the website</h1>
-        <br/>
-        <input type={"text"} onChange={e => setEmail(e.target.value)} />
-        <br/>
-        Already a user? <a href="/login">Login</a>
+            <div style={{
+                paddingTop: 150,
+                marginBottom: 10,
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <Typography variant={"h6"}>
+                Welcome to Coursera. Sign up below
+                </Typography>
+            </div>
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <Card varint={"outlined"} style={{width: 400, padding: 20}}>
+                <TextField
+                    onChange={(evant11) => {
+                        let elemt = evant11.target;
+                        setEmail(elemt.value);
+                    }}
+                    fullWidth={true}
+                    label="Email"
+                    variant="outlined"
+                />
+                <br/><br/>
+                <TextField
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    fullWidth={true}
+                    label="Password"
+                    variant="outlined"
+                    type={"password"}
+                />
+                <br/><br/>
+
+                <Button
+                    size={"large"}
+                    variant="contained"
+                    onClick={() => {
+                        function callback2(data) {
+                            localStorage.setItem("token", data.token);
+                        }
+                        function callback1(res) {
+                            res.json().then(callback2)
+                        }
+                        fetch("http://localhost:3000/admin/signup", {
+                            method: "POST",
+                            body: JSON.stringify({
+                                username: email,
+                                password: password
+                            }),
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        })
+                        .then(callback1)
+                    }}
+
+                > Signup</Button>
+            </Card>
+        </div>
     </div>
 }
 
-export default Register;
+export default Signup;
