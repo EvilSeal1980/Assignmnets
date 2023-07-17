8275234941e8fab0839447a038257062869a4ac2
EvilSeal1980 <evilseal09@gmail.com>
2023-07-17 23:18:00 +0530

    Updated App.jsx

--- week-4/02-medium-course-selling-app-admin-dashboard/src/App.jsx
+++ week-4/02-medium-course-selling-app-admin-dashboard/src/App.jsx
@@ -1,40 +1,27 @@
-import "./App.css"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Landing from "./components/Appbar";
-import CreateCourse from "./components/CreateCourse";
-import Register from "./components/Register";
-import ShowCourses from "./components/ShowCourses";
-import AppBar from "./components/AppBar";
-import { CssBaseline } from "@mui/material";
-import Image from "./assets/image.jpg";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signin from "./Signin.jsx";
+import Signup from "./Signup.jsx";
+import Appbar from "./Appbar.jsx";
+import AddCourse from "./AddCourse.jsx";
 
-// This file shows how you can do routing in React.
-// Try going to /login, /register, /about, /courses on the website and see how the html changes
-// based on the route.
-// You can also try going to /random and see what happens (a route that doesnt exist)
 function App() {
-  return (
-    <>
-      <CssBaseline />
-      <AppBar />
-      <div style={{
-        backgroundColor: "#6133B4",
-        height: "100vh",
-        width: "100%",
-      }}>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/about" element={<CreateCourse />} />
-            <Route path="/courses" element={<ShowCourses />} />
-          </Routes>
-        </Router>
-      </div>
-    </>
-  );
+
+    return (
+        <div style={{width: "100vw",
+            height: "100vh",
+            backgroundColor: "#eeeeee"}}
+        >
+            <Router>
+                <Appbar />
+                <Routes>
+                    <Route path={"/addcourse"} element={
+                        <AddCourse />} />
+                    <Route path={"/signin"} element={<Signin />} />
+                    <Route path={"/signup"} element={<Signup />} />
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
